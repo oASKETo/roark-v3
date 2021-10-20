2857ea4d84d0422f691932c1a9a43a317ad9cd75
Daniel <contact@jiftoo.dev>
2021-10-20 19:08:46 +0300

    Якубович

--- src/components/helper/UserHelper.js
+++ src/components/helper/UserHelper.js
@@ -4,7 +4,7 @@ import "./UserHelper.css";
 export default function UserHelper() {
     return (
         <div className="user-helper-container">
-            
+            <img style={{width: "100%", height: "100%"}} src="/xd.jpg" alt="xd"/>
         </div>
     )
 }
