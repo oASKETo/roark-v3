455f0246efa985c29f1e223c503b0d29ba614932
Daniel <contact@jiftoo.dev>
2021-11-08 15:46:18 +0300

    Опечатка, вынес расчёт высоты в переменную, добавил костыль для фикса высоты

--- src/components/reusable/Collapsible.js
+++ src/components/reusable/Collapsible.js
@@ -1,19 +1,21 @@
 import React, {useRef} from "react";
 
-export default function Collapsible({collapsed, shown, duration = "0.15s", children}) {
+export default function Collapsible({collapsed, maxHeightFix = false, shown, duration = "0.15s", children}) {
 	const ref = useRef();
 
-	const collabsedInternal = collapsed ?? !shown;
+	const collapsedInternal = collapsed ?? !shown;
+
+	const heightCalc = collapsedInternal ? "0" : !ref.current || maxHeightFix ? "2000px" : ref.current.scrollHeight + 200;
 	return (
 		<div
 			ref={ref}
 			className="collapsible"
 			style={{
-                //! +200 is cringe
-				maxHeight: collabsedInternal ? "0" : !ref.current ? "2000px" : (ref.current.scrollHeight + 200),
+				//! +200 is cringe
+				maxHeight: heightCalc,
 				transition: "max-height",
 				transitionDuration: duration,
-				transitionTimingFunction: collabsedInternal ? "ease-out" : "ease-in",
+				transitionTimingFunction: collapsedInternal ? "ease-out" : "ease-in",
 				willChange: "max-height",
 				overflow: "hidden",
 			}}
