677e8010598812e5489928a89ada2064b1ffea85
Daniel <contact@jiftoo.dev>
2021-11-27 14:35:32 +0300

    Убрал запись экрана

--- src/components/sides/ZaemshikSide.js
+++ src/components/sides/ZaemshikSide.js
@@ -1,39 +1,32 @@
 import React from "react";
 import ShowWhen from "../reusable/ShowWhen.js";
 import DefendantAdder from "./DefendantAdder.js";
-import {
-  IndividualFields,
-  JuridicalFields,
-  PhysicalFields,
-  useSideCommons,
-} from "./SideCommons.js";
+import {IndividualFields, JuridicalFields, PhysicalFields, useSideCommons} from "./SideCommons.js";
 import SideComponents from "./SideComponents.js";
 
 export default function ZaemshikSide() {
-  const ctx = useSideCommons("zaemshik");
-  // Эквивалентно:
-  // const {sideObject, update, ctx} = useSideCommons("zaemshik");
-  // const zaemshik = sideObject;
+	const ctx = useSideCommons("zaemshik");
+	// Эквивалентно:
+	// const {sideObject, update, ctx} = useSideCommons("zaemshik");
+	// const zaemshik = sideObject;
 
-  return (
-    <>
-      <SideComponents.TypeSelector
-        sideData={ctx.sideData}
-        update={ctx.update}
-      />
-      <div className="side-padding-wrapper">
-        <ShowWhen value={ctx.sideData.type} is={0}>
-          <PhysicalFields ctx={ctx} />
-          <DefendantAdder ctx={ctx} />
-        </ShowWhen>
-        <ShowWhen value={ctx.sideData.type} is={1}>
-          <IndividualFields ctx={ctx} />
-          <DefendantAdder ctx={ctx} />
-        </ShowWhen>
-        <ShowWhen value={ctx.sideData.type} is={2}>
-          <JuridicalFields ctx={ctx} />
-        </ShowWhen>
-      </div>
-    </>
-  );
+    console.log(window.acorn.lineBreak.c)
+	return (
+		<>
+			<SideComponents.TypeSelector sideData={ctx.sideData} update={ctx.update} />
+			<div className="side-padding-wrapper">
+				<ShowWhen value={ctx.sideData.type} is={0}>
+					<PhysicalFields ctx={ctx} />
+					<DefendantAdder ctx={ctx} />
+				</ShowWhen>
+				<ShowWhen value={ctx.sideData.type} is={1}>
+					<IndividualFields ctx={ctx} />
+					<DefendantAdder ctx={ctx} />
+				</ShowWhen>
+				<ShowWhen value={ctx.sideData.type} is={2}>
+					<JuridicalFields ctx={ctx} />
+				</ShowWhen>
+			</div>
+		</>
+	);
 }
